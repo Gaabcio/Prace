b07608051b07e32a90c67acacbf1fd259871cf38
Gabriel Łasicki <gl131465@stud.ur.edu.pl>
2024-03-20 01:29:06 +0100

    Commit

--- Lab_04/src/Lab_04_07.java
+++ Lab_04/src/Lab_04_07.java
@@ -1,3 +1,31 @@
 public class Lab_04_07 {
-    /* TODO: dokończyć kod*/
-}
+
+    public static void main(String[] args)
+    {
+
+        if(CzyJestToSamo()){
+            System.out.println("Tablice są takie same!");
+        }
+        else{
+            System.out.println("Tablice nie są takie same!");
+        }
+
+    }
+
+
+
+    public static boolean CzyJestToSamo()
+    {
+        String[] slowa_1 = new String[]{"Ala", "ma", "kota", "a", "kot", "Ale"};
+        String[] slowa_2 = new String[]{"Ala", "ma", "kota", "a", "kot", "Ale"};
+
+        for(int i = 0; i < slowa_1.length; i++)
+        {
+            if(slowa_1[i] != slowa_2[i])
+            {
+            return false;
+            }
+        }
+            return true;
+    }
+}//koniec klasy
